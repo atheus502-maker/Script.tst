978620c87491b979f9c248e44d448562f00a7f76
atheus502-maker <atheus502@gmail.com>
2026-01-19 16:56:30 -0300

    Create westbound_b64.lua

---
+++ westbound_b64.lua
@@ -0,0 +1,3 @@
+-- Westbound Full Package (Base64 encoded)
+local code = [[bG9hZHN0...BASE64 COMPLETO AQUI...]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/atheus502-maker/Script.tst/main/westbound_b64.lua"))()
